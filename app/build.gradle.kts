c083412316ee8509400be666d82928aa602ddf3c
alexmncn <118453946+alexmncn@users.noreply.github.com>
2024-12-11 22:55:45 +0100

    FireStore working. Save/Load/Delete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.bom)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/alexmncn/p6_servicios_moviles/HomeActivity.java
+++ app/src/main/java/com/alexmncn/p6_servicios_moviles/HomeActivity.java
@@ -3,11 +3,18 @@ package com.alexmncn.p6_servicios_moviles;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +26,63 @@ public class HomeActivity extends AppCompatActivity {
         String provider = bundle.getString("provider");
 
         Log.d("values", "Email: " + email + " Provider: " + provider);
+
         setup(email, provider);
+        saveData(email);
+        getData(email);
+        deleteData(email);
+    }
+
+    private void saveData(String email) {
+        Button saveButton = findViewById(R.id.saveButton);
+
+        saveButton.setOnClickListener(v -> {
+            TextView providerTextView = findViewById(R.id.providerTextView);
+            EditText addressEditText = findViewById(R.id.addressEditText);
+            EditText numberEditText = findViewById(R.id.numberEditText);
+
+            String provider = providerTextView.getText().toString();
+            String address = addressEditText.getText().toString();
+            String number = numberEditText.getText().toString();
+
+            Map<String, Object> data = new HashMap<>();
+            data.put("provider", provider);
+            data.put("address", address);
+            data.put("number", number);
+
+            db.collection("users").document(email).set(data);
+        });
+    }
+
+    private void getData(String email) {
+        Button getButton = findViewById(R.id.getButton);
+
+        getButton.setOnClickListener(v -> {
+            EditText addressEditText = findViewById(R.id.addressEditText);
+            EditText numberEditText = findViewById(R.id.numberEditText);
+
+            db.collection("users").document(email).get().addOnSuccessListener(documentSnapshot -> {
+               Map<String, Object> data = documentSnapshot.getData();
+
+               if (data != null) {
+                   addressEditText.setText(data.get("address").toString());
+                   numberEditText.setText(data.get("number").toString());
+                }
+            });
+        });
+    }
+
+    private void deleteData(String email) {
+        Button deleteButton = findViewById(R.id.deleteButton);
+
+        deleteButton.setOnClickListener(v -> {
+            EditText addressEditText = findViewById(R.id.addressEditText);
+            EditText numberEditText = findViewById(R.id.numberEditText);
+            addressEditText.setText("");
+            numberEditText.setText("");
+
+            db.collection("users").document(email).delete();
+        });
     }
 
     private void setup(String email, String provider) {
