2db225f6cb0bab23fed208b06a44cadc2b369078
alexmncn <118453946+alexmncn@users.noreply.github.com>
2024-12-11 21:35:25 +0100

    Init

---
+++ app/src/main/java/com/alexmncn/p6_servicios_moviles/MainActivity.java
@@ -0,0 +1,15 @@
+package com.alexmncn.p6_servicios_moviles;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
